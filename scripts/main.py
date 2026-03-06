e7478da8ba94c657ce790c1f3b516aa7d5575ada
KonstantinKondratenko <sider123456789101112131415@gmail.com>
2026-03-06 23:29:02 +0300

    add interesting tests
    add interesting tests in scripts/main.py

--- scripts/main.py
+++ scripts/main.py
@@ -9,3 +9,9 @@ def test_sum_two_numbers():
     assert sum_two_numbers(2, 3) == 5
     assert sum_two_numbers(-1, 1) == 0
 
+    assert sum_two_numbers(-5, -7) == -12
+    assert sum_two_numbers(1.5, 2.5) == 4.0
+
+    with pytest.raises(TypeError):
+        sum_two_numbers("2", 3)
+
